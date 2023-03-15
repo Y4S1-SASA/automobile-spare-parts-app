9f2a0f46daeeb1f043caaa2bdd9727c8806900f2
Avdunusinghe <avdunusinghe@gmail.com>
2023-03-15 08:10:02 +0530

    chore : solution files added

---
+++ lib/theme/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color blueDeep = Color(0xff00a6bb);
+Color grayLight = Color(0xfff6f7fb);
+Color blueDark = Color(0xff3a86FF);
+Color greenDark = Color(0xff006633);
