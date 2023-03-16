85ee1dbe65cbc05da2a170ac603a46868f22562e
amanthajayathilake <amanthajayathilake@gmail.com>
2023-03-16 10:24:07 +0530

    fix: resolved

--- lib/market-items/home.dart
+++ lib/market-items/home.dart
@@ -1,3 +1,4 @@
+import 'package:automobile_spare_parts_app/view/screens/articles/articles-create.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,6 +7,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedAppBarIconIndex = 1;
+
+  void _appBarIconTap(int index) {
+    setState(() {
+      _selectedAppBarIconIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +38,46 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 40,
             ),
-
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Color.fromARGB(255, 58, 140, 54),
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: _selectedAppBarIconIndex == 0
+                  ? Image.asset('assets/appbar/article_filled.png')
+                  : Image.asset('assets/appbar/article.png'),
+              onPressed: () {
+                _appBarIconTap(0);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Scene()),
+                );
+              },
+            ),
+            IconButton(
+              icon: _selectedAppBarIconIndex == 1
+                  ? Image.asset('assets/appbar/market_filled.png')
+                  : Image.asset('assets/appbar/market.png'),
+              onPressed: () => _appBarIconTap(1),
+            ),
+            IconButton(
+              icon: _selectedAppBarIconIndex == 2
+                  ? Image.asset('assets/appbar/reservation_filled.png')
+                  : Image.asset('assets/appbar/reservation.png'),
+              onPressed: () => _appBarIconTap(2),
+            ),
+            IconButton(
+              icon: _selectedAppBarIconIndex == 3
+                  ? Image.asset('assets/appbar/profile_filled.png')
+                  : Image.asset('assets/appbar/profile.png'),
+              onPressed: () => _appBarIconTap(3),
+            ),
           ],
         ),
       ),
