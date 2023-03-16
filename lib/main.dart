6e1c2a77dfbf0e2dc0a424dd927f647b2991b83d
amanthajayathilake <amanthajayathilake@gmail.com>
2023-03-16 10:36:36 +0530

    updated: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:automobile_spare_parts_app/auth/register.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'auth/register.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Color.fromARGB(255, 58, 140, 54),
+        primaryColor: Colors.blue[900],
       ),
       home: RegisterScreen(),
     );
