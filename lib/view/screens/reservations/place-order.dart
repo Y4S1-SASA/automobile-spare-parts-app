ae3e83b6fa62e6443d3ed5887b34572d3b4cb4ec
Dulshan Perera <dulshanperera55@gmail.com>
2023-03-21 23:45:39 +0530

    feat: Added text fields to place order UI

--- lib/view/screens/reservations/place-order.dart
+++ lib/view/screens/reservations/place-order.dart
@@ -1,5 +1,6 @@
 import 'package:automobile_spare_parts_app/view/screens/articles/articles-create.dart';
 import 'package:automobile_spare_parts_app/view/screens/reservations/payment-gateway.dart';
+import 'package:automobile_spare_parts_app/view/screens/reservations/shared/input-field.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,9 @@ class PlaceOrder extends StatefulWidget {
 
 class _PlaceOrderState extends State<PlaceOrder> {
   int _selectedAppBarIconIndex = 1;
+  final TextEditingController quantityController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
 
   void _appBarIconTap(int index) {
     setState(() {
@@ -30,218 +34,221 @@ class _PlaceOrderState extends State<PlaceOrder> {
     double ffem = fem * 0.97;
     return Scaffold(
       // appBar: AppBar(backgroundColor: Colors.green, title: Text('')), // App Bar
-      body: Column(
-        children: [
-          Container(
-            height: 70 * fem,
-            width: double.infinity,
-            color: Colors.green,
-          ),
-          Container(
-            // Heading place order and back arrow
-            margin: EdgeInsets.fromLTRB(33 * fem, 0 * fem, 155 * fem, 0 * fem),
-            width: double.infinity,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(
-                      0 * fem, 0 * fem, 110.35 * fem, 0 * fem),
-                  width: 11.65 * fem,
-                  height: 66 * fem,
-                  child: Image.asset(
-                    'assets/page-1/images/icon-arrow-left-1-3tC.png',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 70 * fem,
+              width: double.infinity,
+              color: Colors.green,
+            ),
+            Container(
+              // Heading place order and back arrow
+              margin:
+                  EdgeInsets.fromLTRB(33 * fem, 0 * fem, 155 * fem, 0 * fem),
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 110.35 * fem, 0 * fem),
                     width: 11.65 * fem,
-                    height: 26 * fem,
+                    height: 66 * fem,
+                    child: Image.asset(
+                      'assets/page-1/images/icon-arrow-left-1-3tC.png',
+                      width: 11.65 * fem,
+                      height: 26 * fem,
+                    ),
                   ),
-                ),
-                Text(
-                  'Place Order',
-                  style: SafeGoogleFont(
-                    'Inter',
-                    fontSize: 24 * ffem,
-                    fontWeight: FontWeight.w500,
-                    height: 1.2125 * ffem / fem,
-                    color: Color(0xff000000),
+                  Text(
+                    'Place Order',
+                    style: SafeGoogleFont(
+                      'Inter',
+                      fontSize: 24 * ffem,
+                      fontWeight: FontWeight.w500,
+                      height: 1.2125 * ffem / fem,
+                      color: Color(0xff000000),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            // Image of the item
-            margin: EdgeInsets.fromLTRB(149 * fem, 0 * fem, 157 * fem, 0 * fem),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8 * fem),
-            ),
-            child: Center(
-              // contentblockdyA (1:79)
-              child: SizedBox(
-                width: double.infinity,
-                height: 139 * fem,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8 * fem),
-                    color: const Color.fromARGB(255, 187, 186, 186),
-                  ),
-                ),
+                ],
               ),
             ),
-          ),
-          Container(
-            // input fields
-            margin:
-                EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32.41 * fem, 22 * fem),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                LabelName(labelName: 'Order Number'),
-                LabelValue(
-                  labelValue: 'ORD-R1297NH-879DNb',
-                  disabled: true,
-                )
-              ],
-            ),
-          ),
-          Container(
-            // input fields
-            margin:
-                EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32.41 * fem, 22 * fem),
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                LabelName(labelName: 'Item Name'),
-                LabelValue(
-                  labelValue: 'Item 1',
-                  disabled: true,
-                )
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32 * fem, 25 * fem),
-            width: double.infinity,
-            height: 79.36 * fem,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin:
-                      EdgeInsets.fromLTRB(0 * fem, 0 * fem, 17 * fem, 0 * fem),
-                  width: 140 * fem,
-                  height: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      LabelName(labelName: 'Quantity'),
-                      LabelValue(labelValue: '2', disabled: false)
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(
-                      0 * fem, 0.36 * fem, 0 * fem, 0 * fem),
-                  width: 224 * fem,
-                  height: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      LabelName(labelName: 'Unit Price(LKR)'),
-                      LabelValue(labelValue: '48500.00', disabled: false)
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32 * fem, 30 * fem),
-            width: double.infinity,
-            height: 79.36 * fem,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin:
-                      EdgeInsets.fromLTRB(0 * fem, 0 * fem, 17 * fem, 0 * fem),
-                  width: 300 * fem,
-                  height: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      LabelName(labelName: 'Delivery Address'),
-                      LabelValue(
-                          labelValue: '110/2B, Kaduwela Road, Kaduwela',
-                          disabled: false)
-                    ],
-                  ),
-                ),
-                Container(
-                  margin:
-                      EdgeInsets.fromLTRB(0 * fem, 17 * fem, 0 * fem, 0 * fem),
-                  width: 50 * fem,
-                  height: 50 * fem,
+            Container(
+              // Image of the item
+              margin:
+                  EdgeInsets.fromLTRB(149 * fem, 0 * fem, 157 * fem, 35 * fem),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8 * fem),
+              ),
+              child: Center(
+                // contentblockdyA (1:79)
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 139 * fem,
                   child: Container(
                     decoration: BoxDecoration(
-                      color: const Color(0xff5db075),
                       borderRadius: BorderRadius.circular(8 * fem),
-                    ),
-                    child: IconButton(
-                      icon:
-                          Image.asset('assets/page-1/images/icon-location.png'),
-                      onPressed: () {},
+                      color: const Color.fromARGB(255, 187, 186, 186),
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(88 * fem, 0 * fem, 87 * fem, 58 * fem),
-            width: double.infinity,
-            height: 45 * fem,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30 * fem),
-            ),
-            child: Container(
+            Container(
+              // input fields
+              margin:
+                  EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32.41 * fem, 22 * fem),
               width: double.infinity,
-              height: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  LabelName(labelName: 'Order Number'),
+                  LabelValue(
+                    labelValue: 'ORD-R1297NH-879DNb',
+                    disabled: true,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              // input fields
+              margin:
+                  EdgeInsets.fromLTRB(32 * fem, 0 * fem, 32.41 * fem, 22 * fem),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  LabelName(labelName: 'Item Name'),
+                  LabelValue(
+                    labelValue: 'Item 1',
+                    disabled: true,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 105 * fem,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 140 * fem,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InputField(
+                            enabled: true,
+                            controller: quantityController,
+                            hint: '2',
+                            labelName: 'Quantity'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 300 * fem,
+                    // height: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InputField(
+                            enabled: true,
+                            controller: priceController,
+                            hint: 'Total',
+                            labelName: 'Total Price')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 105 * fem,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 10 * fem, 0 * fem),
+                    width: 350 * fem,
+                    height: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InputField(
+                            enabled: true,
+                            controller: addressController,
+                            hint: 'Enter Delivery Address',
+                            labelName: 'Delivery Address')
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 0 * fem, 0 * fem, 10 * fem),
+                    width: 50 * fem,
+                    height: 50 * fem,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xff5db075),
+                        borderRadius: BorderRadius.circular(8 * fem),
+                      ),
+                      child: IconButton(
+                        icon: Image.asset(
+                            'assets/page-1/images/icon-location.png'),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  EdgeInsets.fromLTRB(88 * fem, 0 * fem, 87 * fem, 58 * fem),
+              width: double.infinity,
+              height: 45 * fem,
               decoration: BoxDecoration(
-                color: Color(0xff5db075),
                 borderRadius: BorderRadius.circular(30 * fem),
               ),
-              child: MaterialButton(
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                elevation: 2.0,
-                height: 45,
-                minWidth: 270,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const PaymentGateway()),
-                  );
-                },
-                color: const Color(0xff5db075),
-                child: const Text(
-                  "Confirm Order",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontFamily: 'Inter',
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xff5db075),
+                  borderRadius: BorderRadius.circular(30 * fem),
+                ),
+                child: MaterialButton(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  elevation: 2.0,
+                  height: 45,
+                  minWidth: 270,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const PaymentGateway()),
+                    );
+                  },
+                  color: const Color(0xff5db075),
+                  child: const Text(
+                    "Confirm Order",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontFamily: 'Inter',
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
 
       bottomNavigationBar: BottomAppBar(
